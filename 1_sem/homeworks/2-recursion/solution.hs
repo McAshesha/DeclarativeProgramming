6a70dd73a9f89fc90d8732440130b65f594abeed
Ashesha <136958907+McAshesha@users.noreply.github.com>
2024-09-15 22:53:02 +0700

    Homework done on recursions

--- 1_sem/homeworks/2-recursion/solution.hs
+++ 1_sem/homeworks/2-recursion/solution.hs
@@ -1,14 +1,41 @@
-geom_progressia :: Float -> Float -> Float -> Float
-geom_progressia b1 q 1 = b1
-geom_progressia b1 q n = if n<1 then error "Not Positive!" else b1*(q**(n-1))+(geom_progressia b1 q (n-1))
+-- 1.1 переворот числа
+reverseNumber :: Integer -> Integer
+reverseNumber n = f n 0
+    where
+        f 0 rslt = rslt
+        f x rslt = f (x `div` 10) (rslt * 10 + x `mod` 10)
 
-geom_progressia2 :: Float -> Float -> Float
-geom_progressia2 b1 q = if q>(-1) && q<1 then b1/(1-q) else error "q<=-1 or q>=1"
+-- 1.2 проверка на простоту
+isPrime :: Integer -> Bool
+isPrime n
+    | n <= 1 = False
+    | otherwise = f (floor (sqrt (fromIntegral n))) n
+    where
+        f 1 n = True
+        f x n = (n `mod` x) /= 0 && f (x - 1) n
 
-print1 :: Float-> Float
-print1 n = n
+-- 1.3 эффективное фибоначи
+fib :: Integer -> Integer
+fib n = f n 0 1
+  where
+    f 0 a _ = a
+    f k a b = f (k - 1) b (a + b)
 
-geom_progressia3 :: Float -> Float -> Float -> Float -> Float
-geom_progressia3 b1 q n e = if abs(geom_progressia b1 q n - geom_progressia2 b1 q)>=e then geom_progressia3 b1 q (n+1) e else print1 n
+-- 2.1 сумма первых n членов геом прогрессии
+sumGeometric :: Float -> Float -> Float -> Float
+sumGeometric b1 q 1 = b1
+sumGeometric b1 q n
+    | n < 1 = error "Not positive n!"
+    | otherwise = b1 + q * sumGeometric b1 q (n - 1)
 
-f b1 q e = geom_progressia3 b1 q 1 e
+-- 2.2 сумма всех членов геом прогрессии
+sumAllGeometric :: Float -> Float -> Float
+sumAllGeometric b1 q = if abs q < 1 then b1 / (1 - q) else error "q <= -1 or q >= 1"
+
+-- 2.3 наименьшее n суммы ряда геом прогрессии
+getNFromGeometric :: Float -> Float -> Float -> Float
+getNFromGeometric b1 q e = f b1 q e 1
+    where
+        f :: Float -> Float -> Float -> Float -> Float
+        f b1 q e n | abs (sumGeometric b1 q n - sumAllGeometric b1 q) < e = n
+            | otherwise = f b1 q e (n + 1)
--- 1_sem/lessons/2-recursion/solution.hs
+++ 1_sem/lessons/2-recursion/solution.hs
@@ -6,14 +6,16 @@ factorial n = n * factorial (n - 1)
 
 -- 1 двойной факториал
 doubleFactorial :: Int -> Int
-doubleFactorial n | n < 0 = error "Negative n!"
+doubleFactorial n
+    | n < 0 = error "Negative n!"
     | n == 0 || n == 1 = 1
 doubleFactorial 2 = 2
 doubleFactorial n = n * doubleFactorial (n - 2)
 
 -- 2 функция Аккермана
 ak :: Int -> Int -> Int
-ak m n | m == 0 = n + 1
+ak m n
+    | m == 0 = n + 1
     | m > 0 && n == 0 = ak (m - 1) 1
     | m > 0 && n > 0 = ak (m - 1) (ak m (n - 1))
 
@@ -30,21 +32,20 @@ sumCifr 0 = 0
 sumCifr n = n `mod` 10 + sumCifr (n `div` 10)
 
 -- 5 переворот числа
-countCifr :: Int -> Int
-countCifr 0 = 0
-countCifr n = 1 + countCifr (n `div` 10)
-
 reverseNum :: Int -> Int
 reverseNum n | n <= 9 = n
 reverseNum n = (n `div` (10 ^ (countCifr n - 1))) + reverseNum (n `mod` (10 ^ (countCifr n - 1))) * 10
+    where
+        countCifr :: Int -> Int
+        countCifr 0 = 0
+        countCifr n = 1 + countCifr (n `div` 10)
 
 -- 6 обратный отчет ракеты
 countdown :: Int -> String
-countdown 0 = ""
-countdown n = show n ++ ".. " ++ countdown (n - 1)
-
-rocket :: Int -> String
-rocket n = "Ready! " ++ countdown n ++ "Liftoff!"
+countdown n = "Ready! " ++ cdwn n ++ "Liftoff!"
+    where
+        cdwn 0 = ""
+        cdwn n = show n ++ ".. " ++ cdwn (n - 1)
 
 -- 7* количество пересечений
 count :: Int -> Int
